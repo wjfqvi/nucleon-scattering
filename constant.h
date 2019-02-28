1661275c8be779ddb9485bbede88161ebea73681
WJF <wjfsamuel@outlook.com>
2019-02-28 22:54:31 +0800

    add constant.h

---
+++ constant.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANT_H_
+#define CONSTANT_H_
+
+#include<complex>
+using namespace std;
+
+const double Pi = 3.1415926535897932384626433L;
+const double PiSqr = 1.772453850905516L;
+const double MevToBarn = 389387.0;
+const double DToR = Pi/180.0;
+const double mN = 938.0;
+const complex<double> iUnit = complex<double>(0,1);
+
+#endif
